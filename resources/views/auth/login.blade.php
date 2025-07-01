6708b55bb31ece0360b097a5dbdf0565c4672708
backeamin <backeamin@gmail.com>
2025-07-01 17:42:50 +0600

    all done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
         </div>
 
         <div class="form-group mb-3">
-            <a href="{{ route('password.request') }}" class="text-muted float-right"><small>Forgot your password?</small></a>
+{{--            <a href="{{ route('password.request') }}" class="text-muted float-right"><small>Forgot your password?</small></a>--}}
 
             <label for="password">Password</label>
             <input id="password" type="password" placeholder="Enter your password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -30,12 +30,12 @@
             @enderror
         </div>
 
-        <div class="form-group mb-3">
-            <div class="custom-control custom-checkbox">
-                <input class="custom-control-input" type="checkbox" name="remember" id="checkbox-signin" {{ old('remember') ? 'checked' : '' }}>
-                <label class="custom-control-label" for="checkbox-signin">Remember me</label>
-            </div>
-        </div>
+{{--        <div class="form-group mb-3">--}}
+{{--            <div class="custom-control custom-checkbox">--}}
+{{--                <input class="custom-control-input" type="checkbox" name="remember" id="checkbox-signin" {{ old('remember') ? 'checked' : '' }}>--}}
+{{--                <label class="custom-control-label" for="checkbox-signin">Remember me</label>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
         <div class="form-group mb-0 text-center">
             <button class="btn btn-gradient btn-block" type="submit"> Log In </button>
@@ -43,10 +43,10 @@
 
     </form>
 
-    <div class="row mt-4">
-        <div class="col-sm-12 text-center">
-            <p class="text-muted mb-0">Don't have an account? <a href="{{ route('register') }}" class="text-dark ml-1"><b>Sign Up</b></a></p>
-        </div>
-    </div>
+{{--    <div class="row mt-4">--}}
+{{--        <div class="col-sm-12 text-center">--}}
+{{--            <p class="text-muted mb-0">Don't have an account? <a href="{{ route('register') }}" class="text-dark ml-1"><b>Sign Up</b></a></p>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 @endsection
 
