45bd8364f7884b9095c7364a2f484c1a7d2ae47b
Saidul Islam <saidulislam49du@gmail.com>
2025-08-04 12:37:35 +0600

    second photo card added

--- resources/views/admin/post/generate_photo_cut.blade.php
+++ resources/views/admin/post/generate_photo_cut.blade.php
@@ -1,22 +1,27 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="{{asset('frontend/assets')}}/css/tailwind_css/tailwind_output.css" rel="stylesheet">
+    <link href="{{ asset('frontend/assets') }}/css/tailwind_css/tailwind_output.css" rel="stylesheet">
     <title>Generate Photo Cut</title>
     <style>
-        .post_generate_wrapp{
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+        .post_generate_wrapp {
             width: 720px;
             margin: 25px auto;
             position: relative;
         }
-        .frame_img{
+
+        .post_generate_wrapp .frame_img {
             width: 100%;
         }
-        .date {
+
+        .post_generate_wrapp .date {
             color: #ffffff;
             display: inline-block;
             position: absolute;
@@ -25,7 +30,8 @@
             font-size: 20px;
             font-weight: 600;
         }
-        .post_thumbnail {
+
+        .post_generate_wrapp .post_thumbnail {
             width: 670px;
             height: 353px;
             background: red;
@@ -33,11 +39,13 @@
             top: 53px;
             left: 25px;
         }
-        .post_thumbnail .post_thumbnail_img{
+
+        .post_generate_wrapp .post_thumbnail .post_thumbnail_img {
             width: 100%;
             height: 100%;
         }
-        .title_wrapp {
+
+        .post_generate_wrapp .title_wrapp {
             width: 95.7%;
             height: 235px;
             position: absolute;
@@ -48,7 +56,8 @@
             justify-content: center;
             align-items: center;
         }
-        .title{
+
+        .post_generate_wrapp .title {
             font-size: 35px;
             color: #ffffff;
             text-transform: uppercase;
@@ -56,7 +65,8 @@
             font-weight: 800;
             padding: 20px;
         }
-        .check_in_comment {
+
+        .post_generate_wrapp .check_in_comment {
             color: #ffffff;
             font-size: 18px;
             position: absolute;
@@ -64,7 +74,7 @@
             bottom: 70px;
         }
 
-        .button_group button {
+        .post_generate_wrapp .button_group button {
             padding: 8px 16px;
             margin: 0 5px;
             font-size: 16px;
@@ -74,24 +84,80 @@
             border: none;
             border-radius: 6px;
         }
-        .button_group button:hover {
+
+        .post_generate_wrapp .button_group button:hover {
             background-color: #0056b3;
         }
 
+        .post_generate_wrapp_2 .title {
+            color: #151a58;
+            font-family: "Noto Serif Bengali";
+        }
+
+        .post_generate_wrapp_2 .date {
+            left: 15px;
+            top: 94.5%;
+            color: #000000;
+        }
+
+        .post_generate_wrapp_2 .post_thumbnail {
+            width: 720px;
+            height: 408px;
+            position: absolute;
+            top: 0px;
+            left: 0px;
+        }
+
+        .post_generate_wrapp_2 .brand_title {
+            background: #ede7e7;
+            width: 95px;
+            height: 116px;
+            border-radius: 0 0 100px 100px;
+            position: absolute;
+            top: 0;
+            right: 15px;
+            padding-top: 10px;                        
+        }
+
+        .post_generate_wrapp_2 h3 {
+            color: #151a58;
+            text-transform: uppercase;
+            font-size: 22px;
+            font-weight: 800;
+            line-height: 1.2;
+            text-align: center;
+            font-family: "Poppins", sans-serif;
+        }
     </style>
 </head>
+
 <body>
 
     <div class="post_generate_wrapp">
-        <img src="{{asset('frontend/assets/image/Photo-fream.png')}}" class="frame_img" alt="frame image">
-        <span class="date">{{ bangla_date_from_iso($post->publishing_date??null) }}</span>
+        <img src="{{ asset('frontend/assets/image/Photo-fream.png') }}" class="frame_img" alt="frame image">
+        <span class="date">{{ bangla_date_from_iso($post->publishing_date ?? null) }}</span>
         <div class="post_thumbnail">
-            <img src="{{asset('storage')}}/{{$post->media->image??null}}" class="post_thumbnail_img" alt="thumbnail">
+            <img src="{{ asset('storage') }}/{{ $post->media->image ?? null }}" class="post_thumbnail_img"
+                alt="thumbnail">
         </div>
         <div class="title_wrapp">
-            <h3 class="title">{{$post->title??null}}</h3>
+            <h3 class="title">{{ $post->title ?? null }}</h3>
         </div>
         <h5 class="check_in_comment">বিস্তারিত কমেন্টে দেখুন</h5>
+    </div>
+    <div class="post_generate_wrapp post_generate_wrapp_2">
+        <img src="{{ asset('frontend/assets/image/photo-frame-2.png') }}" class="frame_img" alt="frame image">
+        <span class="date">{{ bangla_date_from_iso($post->publishing_date ?? null) }}</span>
+        <div class="post_thumbnail">
+            <img src="{{ asset('storage') }}/{{ $post->media->image ?? null }}" class="post_thumbnail_img"
+                alt="thumbnail">
+        </div>
+        <div class="title_wrapp">
+            <h3 class="title">{{ $post->title ?? null }}</h3>
+        </div>
+        <div class="brand_title">
+            <h3>The dhaka diary</h3>
+        </div>
     </div>
     <div class="button_group" style="margin-top: 10px; text-align:center;">
         <button id="downloadBtn">Download Photo</button>
@@ -125,7 +191,9 @@
         document.getElementById('copyBtn').addEventListener('click', () => {
             captureHighRes(canvas => {
                 canvas.toBlob(blob => {
-                    const item = new ClipboardItem({ 'image/png': blob });
+                    const item = new ClipboardItem({
+                        'image/png': blob
+                    });
                     navigator.clipboard.write([item])
                         .then(() => alert('Image copied to clipboard!'))
                         .catch(err => alert('Failed to copy image: ' + err));
@@ -135,4 +203,5 @@
     </script>
 
 </body>
+
 </html>
